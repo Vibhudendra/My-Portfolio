212e09ec7f2ef4b84d7e8656d3cd3159eb47403e
unknown <vibhudendra.s@gmail.com>
2024-06-22 18:36:10 +0530

    Initial Commit

---
+++ src/assets/images/index.js
@@ -0,0 +1,19 @@
+import hero from './hero.jpg'
+import meta from './meta.png'
+import shopify from './shopify.png'
+import starbucks from './starbucks.png'
+import tesla from './tesla.png'
+import logo from './logo.svg'
+import TCS from './TCS.png'
+import WIRIN from './WIRIN.jpg'
+
+export {
+    hero,
+    meta,
+    shopify,
+    starbucks,
+    tesla,
+    logo,
+    TCS,
+    WIRIN
+}
---
+++ src/constants/index.js
@@ -0,0 +1,183 @@
+import { meta, shopify, starbucks, tesla, TCS, WIRIN } from "../assets/images";
+import {
+    car,
+    contact,
+    css,
+    estate,
+    express,
+    git,
+    github,
+    html,
+    javascript,
+    linkedin,
+    mongodb,
+    motion,
+    mui,
+    nextjs,
+    nodejs,
+    pricewise,
+    react,
+    redux,
+    sass,
+    snapgram,
+    summiz,
+    tailwindcss,
+    threads,
+    Zustand,
+    typescript
+} from "../assets/icons";
+
+export const skills = [
+    {
+        imageUrl: css,
+        name: "CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: git,
+        name: "Git",
+        type: "Version Control",
+    },
+    {
+        imageUrl: github,
+        name: "GitHub",
+        type: "Version Control",
+    },
+    {
+        imageUrl: html,
+        name: "HTML",
+        type: "Frontend",
+    },
+    {
+        imageUrl: javascript,
+        name: "JavaScript",
+        type: "Frontend",
+    },
+    {
+        imageUrl: mongodb,
+        name: "MongoDB",
+        type: "Database",
+    },
+    {
+        imageUrl: motion,
+        name: "Motion",
+        type: "Animation",
+    },
+    {
+        imageUrl: mui,
+        name: "Material-UI",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nextjs,
+        name: "Next.js",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nodejs,
+        name: "Node.js[Familiar]",
+        type: "Backend",
+    },
+    {
+        imageUrl: react,
+        name: "React",
+        type: "Frontend",
+    },
+    {
+        imageUrl: redux,
+        name: "Redux",
+        type: "State Management",
+    },
+    {
+        imageUrl: sass,
+        name: "Sass",
+        type: "Frontend",
+    },
+    {
+        imageUrl: tailwindcss,
+        name: "Tailwind CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: typescript,
+        name: "TypeScript",
+        type: "Frontend",
+    }
+];
+
+export const experiences = [
+    {
+        title: "Software Developer",
+        company_name: "Tata Consultancy services",
+        icon: TCS,
+        iconBg: "#accbe1",
+        date: "June 2023 - Present",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+        ],
+    },
+    {
+        title: "Inter UI Developer",
+        company_name: "WIRIN",
+        icon: WIRIN,
+        iconBg: "#fbc3bc",
+        date: "Dec 2022 - Mar 2023",
+        points: [
+            "Collaborated on research and design to conceptualize and refine the digital cockpit interface for an autonomous vehicle.",
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+        ],
+    },
+];
+
+export const socialLinks = [
+    {
+        name: 'Contact',
+        iconUrl: contact,
+        link: '/contact',
+    },
+    {
+        name: 'GitHub',
+        iconUrl: github,
+        link: 'https://github.com/Vibhudendra',
+    },
+    {
+        name: 'LinkedIn',
+        iconUrl: linkedin,
+        link: 'https://www.linkedin.com/in/vibhudendras/',
+    }
+];
+
+export const projects = [
+    {
+        iconUrl: car,
+        theme: 'btn-back-blue',
+        name: 'Car Finding App',
+        description: 'Designed and built a mobile app for finding and comparing cars on the market, streamlining the car-buying process.',
+        link: 'https://github.com/Vibhudendra/cars_showcase',
+    },
+    {
+        iconUrl: estate,
+        theme: 'btn-back-yellow',
+        name: 'Real-Estate Application',
+        description: 'Designed and built a Realestates website for finding reliable homes on the market, Simplifying the property purchase and rent process',
+        link: 'https://github.com/Vibhudendra/Homes-RealEstate',
+    },
+    {
+        iconUrl: summiz,
+        theme: 'btn-back-pink',
+        name: 'Portfolio ',
+        description: 'Designed and built portfolio website that showcases projects, skills and experience.',
+        link: 'https://github.com/Vibhudendra/Portfolio',
+    },
+    {
+        iconUrl: summiz,
+        theme: 'btn-back-yellow',
+        name: 'AI Summarizer Application',
+        description: 'App that leverages AI to automatically generate concise & informative summaries from lengthy text content, or blogs.',
+        link: 'InProgress'
+    }
+];
